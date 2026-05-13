9d37263765f03f58e9343f6ebd645c739a2d0112
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2026-05-13 17:28:59 +0200

    Fix: transforms.py force nearest-neighbor across all stages for registering segmentations

--- guidereg/core/transforms.py
+++ guidereg/core/transforms.py
@@ -1,5 +1,6 @@
 import itk
 
+
 def apply_transform_to_segmentation(
     segmentation,
     transform_parameter_object
@@ -7,21 +8,35 @@ def apply_transform_to_segmentation(
 
     # ======================================
     # FORCE NEAREST-NEIGHBOR
+    # ACROSS ALL STAGES
     # ======================================
-    parameter_map = (
+    number_of_maps = (
         transform_parameter_object
-        .GetParameterMap(0)
+        .GetNumberOfParameterMaps()
     )
 
-    parameter_map["FinalBSplineInterpolationOrder"] = ["0"]
-    transform_parameter_object.SetParameterMap(0, parameter_map)
+    for i in range(number_of_maps):
+
+        parameter_map = (
+            transform_parameter_object
+            .GetParameterMap(i)
+        )
+        parameter_map["FinalBSplineInterpolationOrder"] = ["0"]
+
+        transform_parameter_object.SetParameterMap(
+            i,
+            parameter_map
+        )
 
     # ======================================
     # APPLY TRANSFORM
     # ======================================
     result = itk.transformix_filter(
+
         segmentation,
-        transform_parameter_object = transform_parameter_object
+
+        transform_parameter_object=
+        transform_parameter_object
     )
 
     return result
