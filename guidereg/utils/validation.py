d7608b680081404bdb154fee501af6b74b807117
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2026-05-22 15:17:15 +0200

    Add: validation for requiered information from the full pipeline and propagation pipeline in validation.py

--- guidereg/utils/validation.py
+++ guidereg/utils/validation.py
@@ -1,43 +1,96 @@
 from pathlib import Path
 
 
-REQUIRED_TOP_LEVEL_KEYS = [
-    "dataset",
-    "columns",
-    "output",
-    "registration"
-]
+def validate_config(
+    config: dict,
+    task: str
+):
 
+    # ======================================
+    # TASK-SPECIFIC REQUIRED KEYS
+    # ======================================
+    if task == "pipeline":
 
-def validate_config(config: dict):
-    # ==========================================
-    # CHECK REQUIRED SECTIONS
-    # ==========================================
-    for key in REQUIRED_TOP_LEVEL_KEYS:
-        if key not in config:
-            raise ValueError(
-                f"Missing required configuration section: '{key}'"
-            )
+        required_keys = [
 
-    # ==========================================
-    # CHECK INPUT CSV
-    # ==========================================
-    input_csv = Path(config["dataset"]["input_csv"])
-    if not input_csv.exists():
-        raise FileNotFoundError(
-            f"Input CSV not found: {input_csv}"
+            "dataset",
+            "columns",
+            "output",
+            "registration"
+        ]
+
+    elif task == "propagate":
+
+        required_keys = [
+            "dataset",
+            "columns",
+            "output",
+            "propagation"
+        ]
+
+    else:
+
+        raise ValueError(
+            f"Unknown task: {task}"
         )
 
-    # ==========================================
-    # CHECK REGISTRATION STAGES
-    # ==========================================
-    valid_stages = [
-        "initial",
-        "rigid"
-    ]
-    stages = config["registration"]["stages"]
-    for stage in stages:
-        if stage not in valid_stages:
+    # ======================================
+    # CHECK REQUIRED SECTIONS
+    # ======================================
+    for key in required_keys:
+        if key not in config:
             raise ValueError(
-                f"Invalid registration stage: '{stage}'"
+
+                f"Missing required "
+                f"configuration section: "
+                f"'{key}'"
             )
+
+    # ======================================
+    # CHECK INPUT CSV
+    # ======================================
+    input_csv = Path(
+        config["dataset"]["input_csv"]
+    )
+
+    if not input_csv.exists():
+        raise FileNotFoundError(
+            f"Input CSV not found: "
+            f"{input_csv}"
+        )
+
+    # ======================================
+    # REGISTRATION VALIDATION
+    # ======================================
+    if task == "pipeline":
+        valid_stages = [
+            "initial",
+            "rigid"
+        ]
+
+        stages = config["registration"]["stages"]
+
+        for stage in stages:
+            if stage not in valid_stages:
+                raise ValueError(
+                    f"Invalid registration "
+                    f"stage: '{stage}'"
+                )
+
+    # ======================================
+    # PROPAGATION VALIDATION
+    # ======================================
+    elif task == "propagate":
+        valid_stages = [
+            "initial",
+            "rigid"
+        ]
+
+        stages = config["propagation"]["stages"]
+
+        for stage in stages:
+            if stage not in valid_stages:
+                raise ValueError(
+                    f"Invalid propagation "
+                    f"stage: '{stage}'"
+                )
